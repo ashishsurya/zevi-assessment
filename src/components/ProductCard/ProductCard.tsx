5ee5e1717aaad990ef211f4fc6236e17ecdbf1dd
Surya Ashish <ashishsurya126@gmail.com>
2023-07-29 02:44:50 +0530

    filtering bugs fixed and view product added

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,18 +1,33 @@
-import { FC } from 'react';
+import { ElementRef, FC, useRef } from 'react';
 import './ProductCard.scss';
 import { Product } from '../../utils/types';
 import { Rating } from '@smastrom/react-rating';
+import { useHover } from 'react-haiku';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface ProductCardProps {
   product: Product;
 }
 
 export const ProductCard: FC<ProductCardProps> = ({ product }) => {
+  const { ref: productRef, hovered } = useHover<ElementRef<'div'>>();
   const discountPrice = product.price - 200;
   return (
-    <div className='product'>
-      
-      <img src={product.img} alt='product-title' className='product__image' />
+    <div className='product' ref={productRef}>
+      <div className='product__image'>
+        <img src={product.img} alt='product-title' />
+        <AnimatePresence>
+          {hovered && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <p>View product</p>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
       <h4 className='product__name'>{product.name}</h4>
       <p className='product__price'>
         <span className='product__price__actual'>Rs. {product.price}</span>{' '}
--- src/components/pages/SearchPage.tsx
+++ src/components/pages/SearchPage.tsx
@@ -1,13 +1,12 @@
-import { FC } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import './SearchPage.scss';
-import { SearchBar } from '../SearchBar/SearchBar';
-import { Logo } from '../Logo/Logo';
-import { Filters } from '../Filters/Filters';
-import { Products } from '../Products/Products';
-import { useProducts } from '../../zustand/useProducts';
-import { useFilterStore } from '../../zustand/useFilter';
+import { FC, useCallback } from 'react';
 import { Product } from '../../utils/types';
+import { useFilterStore } from '../../zustand/useFilter';
+import { useProducts } from '../../zustand/useProducts';
+import { Filters } from '../Filters/Filters';
+import { Logo } from '../Logo/Logo';
+import { Products } from '../Products/Products';
+import { SearchBar } from '../SearchBar/SearchBar';
+import './SearchPage.scss';
 
 interface SearchPageProps {}
 
@@ -15,22 +14,37 @@ export const SearchPage: FC<SearchPageProps> = () => {
   const { products } = useProducts();
   const { brands, ratings } = useFilterStore();
 
-  const getBrands = () => {
+  const getBrands = useCallback(() => {
     return Array.from(new Set(products.map((product) => product.brand)));
-  };
+  }, [products]);
 
-  const filteredProducts = (products: Product[]) => {
-    if (brands.length === 0 && ratings.length === 0) {
-      return products;
-    }
+  const filteredProducts = useCallback(
+    (products: Product[]) => {
+      if (brands.length === 0 && ratings.length === 0) {
+        return products;
+      }
+      if (brands.length === 0 || ratings.length === 0) {
+        if (brands.length === 0) {
+          return products.filter((product) => {
+            return ratings.includes(product.rating);
+          });
+        }
 
-    return products.filter((product) => {
-      return (
-        (brands.length > 0 && brands.includes(product.brand)) ||
-        (ratings.length > 0 && ratings.includes(product.rating))
-      );
-    });
-  };
+        if (ratings.length === 0) {
+          return products.filter((product) => {
+            return brands.includes(product.brand);
+          });
+        }
+      }
+
+      return products.filter((product) => {
+        return (
+          brands.includes(product.brand) && ratings.includes(product.rating)
+        );
+      });
+    },
+    [brands, ratings]
+  );
 
   return (
     <div className='searchpage__wrapper'>
